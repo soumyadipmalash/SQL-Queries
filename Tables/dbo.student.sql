8c25180ac50f0319c71d685b5149e6968f6155b0
Soumyadip Malash <soumyadipmalash@gmail.com>
2025-08-13 14:46:01 +0530

    initial commit

---
+++ Tables/dbo.student.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[student] (
+  [Rollno] [int] NULL,
+  [FirstName] [varchar](50) NULL,
+  [LastName] [varchar](50) NULL,
+  [Marks] [int] NULL,
+  [Address] [varchar](100) NULL,
+  [City] [varchar](50) NULL,
+  [cgpa] [numeric](3, 2) NULL,
+  [Age] [int] NULL,
+  [Branch] [varchar](4) NULL
+)
+ON [PRIMARY]
+GO
